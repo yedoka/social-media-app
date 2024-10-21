5d8161956eae7863841fd3d007927d9a604f079f
yedoka <132571700+yedoka@users.noreply.github.com>
2024-10-21 21:37:46 +0200

    created User and Credentials types

---
+++ src/types/Credentials.ts
@@ -0,0 +1,11 @@
+export interface User {
+  username: string;
+  avatar: string;
+}
+
+export interface Credentials {
+  email: string;
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
