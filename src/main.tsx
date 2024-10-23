f398c6dd8d0b16cc1ac45741c1857cf1691345e2
yedoka <132571700+yedoka@users.noreply.github.com>
2024-10-23 17:42:32 +0200

    Deleted redux store to start from zero

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,12 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import {routes} from "./constants/router";
-import store from "./store/index";
-import { Provider } from "react-redux";
 import "./styles/globals.scss";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
