37dc6ca9efb8c2f5f469e217300828de9b66da4b
yedoka <akedilbekbolatov@gmail.com>
2024-11-24 20:40:36 +0100

    new auth types

---
+++ src/types/auth.ts
@@ -0,0 +1,10 @@
+export interface SignInFormInputs {
+  email: string;
+  password: string;
+}
+
+export interface SignUpFormInputs {
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
