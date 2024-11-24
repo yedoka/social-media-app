a261b09729494eb95a9a4fb543b0a143ef340e3a
yedoka <akedilbekbolatov@gmail.com>
2024-11-24 20:36:36 +0100

    moved import

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import {routes} from "./constants/router";
-import "./styles/globals.scss";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import "./styles/globals.scss";
 
 const router = createBrowserRouter(routes);
 
