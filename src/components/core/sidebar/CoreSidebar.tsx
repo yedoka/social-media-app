2eee232240481c7baff5be4730de4d58212a6a1e
yedoka <akedilbekbolatov@gmail.com>
2024-11-24 20:39:01 +0100

    added log out button and new classnames

--- src/components/core/sidebar/CoreSidebar.tsx
+++ src/components/core/sidebar/CoreSidebar.tsx
@@ -1,20 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../../services/firebase";
+import { signOut } from "firebase/auth";
+import Button from "../Button";
 import "./CoreSidebar.scss";
 
 const CoreSidebar = () => {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      navigate('/sign-in');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
-    <div className="coreSidebar">
-      <h1>SoundShare</h1>
+    <div className="sidebar">
+      <h1>Social Media</h1>
       <nav>
         <ul>
           <li>
-            <Link to={`/`}>Feed</Link>
+            <Link to={`/`} className="sidebar__link">Feed</Link>
           </li>
           <li>
-            <Link to={`/profile`}>Profile</Link>
+            <Link to={`/profile`} className="sidebar__link">Profile</Link>
           </li>
         </ul>
       </nav>
+      <Button onClick={logout}>Log out</Button>
     </div>
   )
 }
