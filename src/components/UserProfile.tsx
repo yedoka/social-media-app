7f333a6174d79474bc5f9882d85c4b3282a40fea
yedoka <akedilbekbolatov@gmail.com>
2024-11-24 20:36:20 +0100

    removed profile editing logic for mock backend

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,39 +1,12 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../store/store';
-import { updateProfile } from '../store/slices/profileSlice';
-import ProfileForm from './ProfileForm';
-import ProfileData from './ProfileData';
-import './UserProfile.scss';
+import { auth } from '../services/firebase'; 
 
-const UserProfile = () => {
-  const user = useSelector((state: RootState) => state.profile.userDetails);
-  const dispatch = useDispatch();
+const UserProfile: React.FC = () => {
 
-  const [isEditing, setIsEditing] = useState(false);
-
-  const handleSave = (formData: NonNullable<typeof user>) => {
-    dispatch(updateProfile(formData));
-    setIsEditing(false);
-  };
-
-  const handleCancel = () => {
-    setIsEditing(false);
-  };
-
-  if (!user) {
-    return <div>Loading...</div>;
+  if (auth) {
+    return <div>Signed in as: {auth?.currentUser?.email}</div>;
+  } else {
+    return <div>No user is signed in.</div>;
   }
-
-  return (
-    <div className="user-details">
-      {isEditing ? (
-        <ProfileForm user={user} onSave={handleSave} onCancel={handleCancel} />
-      ) : (
-        <ProfileData user={user} onEdit={() => setIsEditing(true)} />
-      )}
-    </div>
-  );
 };
 
 export default UserProfile;
