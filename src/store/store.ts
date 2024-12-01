f447189219c6fff6036aa7ca34b91f432df95dff
yedoka <akedilbekbolatov@gmail.com>
2024-12-01 11:02:56 +0100

    added: new slices for auth and profile edit

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from './slices/authSlice'
+import authReducer from './slices/auth';
+import editProfileReducer from './slices/editProfile';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    editProfile: editProfileReducer,
   },
 });
 
