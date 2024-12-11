18e3cc1c4638722860bf764552b6b1ca0cdb7ef1
yedoka <akedilbekbolatov@gmail.com>
2024-12-11 22:19:47 +0100

    adding new user to firestore

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router-dom';
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { auth } from '../../services/firebase';
-import { authenticate } from '../../store/slices/auth';
-import { useDispatch } from 'react-redux';
-import { FirebaseError } from 'firebase/app';
-import type { SignUpFormInputs } from '../../types/auth';
-import './SignUp.scss';
-import Button from '../../components/core/Button';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../../services/firebase";
+import { authenticate } from "../../store/slices/auth";
+import { useDispatch } from "react-redux";
+import { FirebaseError } from "firebase/app";
+import type { SignUpFormInputs } from "../../types/auth";
+import Button from "../../components/core/Button";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../services/firebase";
+import "./SignUp.scss";
 
 const SignUpForm: React.FC = () => {
   const {
@@ -20,47 +22,53 @@ const SignUpForm: React.FC = () => {
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   const [error, setError] = React.useState<string | null>(null);
 
   const onSubmit = async (data: SignUpFormInputs) => {
     const { email, password, passwordConfirmation, displayName } = data;
 
     if (password !== passwordConfirmation) {
-      setError('Passwords do not match!');
+      setError("Passwords do not match!");
       return;
     }
 
     try {
       setError(null);
-
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       if (userCredential.user) {
         await updateProfile(userCredential.user, {
           displayName,
         });
       }
+      const user = userCredential.user;
+      const uid = user.uid;
 
-      // Dispatch authentication action and navigate
+      const userRef = doc(db, "users", uid);
+      await setDoc(userRef, {
+        email: email,
+        displayName: displayName,
+        profilePicture:
+          "https://i.pinimg.com/236x/68/31/12/68311248ba2f6e0ba94ff6da62eac9f6.jpg",
+      });
+
       dispatch(authenticate());
-      navigate('/');
+      navigate("/");
     } catch (err) {
       if (err instanceof FirebaseError) {
         switch (err.code) {
-        case 'auth/email-already-in-use':
-          setError('This email is already in use. Please try another.');
-          break;
-        case 'auth/invalid-email':
-          setError('Invalid email. Please enter a valid email address.');
-          break;
-        case 'auth/weak-password':
-          setError('Weak password. Password must be at least 6 characters.');
+        case "auth/email-already-in-use":
+          setError("This email is already in use. Please try another.");
           break;
         default:
-          setError('An error occurred. Please try again later.');
+          setError("An error occurred. Please try again later.");
         }
       } else {
-        setError('An unknown error occurred.');
+        setError("An unknown error occurred.");
       }
     }
   };
@@ -72,24 +80,30 @@ const SignUpForm: React.FC = () => {
         <label className="signUpForm__label">Display Name</label>
         <input
           type="text"
-          className={`signUpForm__input ${errors.displayName ? 'signUpForm__input--error' : ''}`}
+          className={`signUpForm__input ${
+            errors.displayName ? "signUpForm__input--error" : ""
+          }`}
           placeholder="Enter your display name"
-          {...register('displayName', {
-            required: 'Display name is required',
+          {...register("displayName", {
+            required: "Display name is required",
           })}
         />
-        {errors.displayName && <p className="errorMessage">{errors.displayName.message}</p>}
+        {errors.displayName && (
+          <p className="errorMessage">{errors.displayName.message}</p>
+        )}
 
         <label className="signUpForm__label">Email</label>
         <input
           type="email"
-          className={`signUpForm__input ${errors.email ? 'signUpForm__input--error' : ''}`}
+          className={`signUpForm__input ${
+            errors.email ? "signUpForm__input--error" : ""
+          }`}
           placeholder="Enter your email"
-          {...register('email', {
-            required: 'Email is required',
+          {...register("email", {
+            required: "Email is required",
             pattern: {
               value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: 'Enter a valid email',
+              message: "Enter a valid email",
             },
           })}
         />
@@ -98,29 +112,33 @@ const SignUpForm: React.FC = () => {
         <label className="signUpForm__label">Password</label>
         <input
           type="password"
-          className={`signUpForm__input ${errors.password ? 'signUpForm__input--error' : ''}`}
+          className={`signUpForm__input ${
+            errors.password ? "signUpForm__input--error" : ""
+          }`}
           placeholder="Enter your password"
-          {...register('password', {
-            required: 'Password is required',
+          {...register("password", {
+            required: "Password is required",
             minLength: {
               value: 6,
-              message: 'Password must be at least 6 characters',
+              message: "Password must be at least 6 characters",
             },
           })}
         />
-        {errors.password && <p className="errorMessage">{errors.password.message}</p>}
+        {errors.password && (
+          <p className="errorMessage">{errors.password.message}</p>
+        )}
 
         <label className="signUpForm__label">Confirm Password</label>
         <input
           type="password"
           className={`signUpForm__input ${
-            errors.passwordConfirmation ? 'signUpForm__input--error' : ''
+            errors.passwordConfirmation ? "signUpForm__input--error" : ""
           }`}
           placeholder="Confirm your password"
-          {...register('passwordConfirmation', {
-            required: 'Password confirmation is required',
+          {...register("passwordConfirmation", {
+            required: "Password confirmation is required",
             validate: (value) =>
-              value === watch('password') || 'Passwords do not match',
+              value === watch("password") || "Passwords do not match",
           })}
         />
         {errors.passwordConfirmation && (
