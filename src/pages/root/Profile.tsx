deb13c087146f48d72bb75f323f36afdcfe7914b
yedoka <akedilbekbolatov@gmail.com>
2024-12-13 18:29:42 +0100

    semantic adjustment

--- src/pages/root/Profile.tsx
+++ src/pages/root/Profile.tsx
@@ -1,9 +1,12 @@
 import UserProfile from "../../components/core/userProfile/UserProfile";
 
 const Profile = () => {
-  return <main>
-    <UserProfile />
-  </main>
+
+  return (
+    <main>
+      <UserProfile />
+    </main>
+  );
 };
 
 export default Profile;
