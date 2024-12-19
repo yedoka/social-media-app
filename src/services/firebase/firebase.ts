ec99a601d49ca660476edcb4bd33de24ea5e6e50
yedoka <akedilbekbolatov@gmail.com>
2024-12-19 17:33:31 +0100

    fixed security problem

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJGxsgRDyLu9L0Ea3-MvkOK_sQ5pv8AVA",
-  authDomain: "social-media-app-952de.firebaseapp.com",
-  projectId: "social-media-app-952de",
-  storageBucket: "social-media-app-952de.firebasestorage.app",
-  messagingSenderId: "813268098629",
-  appId: "1:813268098629:web:bc6276a53627b3bd724d2c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
