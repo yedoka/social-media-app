a0d355d08107a3149e35a32f7431215f3fe1b370
yedoka <akedilbekbolatov@gmail.com>
2025-07-28 11:39:49 +0200

    feat: add loading spinner during authentication check and wrap App component with AppProviders

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -6,47 +6,53 @@ import { useAuthStore } from "@/features/auth/model/useAuthStore";
 import { Auth, Feed, Profile, Messages } from "@/pages";
 import { AuthLayout, RootLayout } from "@/layout";
 
-import { AppProviders } from "./AppProviders";
+import { Center, Spinner } from "@chakra-ui/react";
 
 export const App = () => {
-  const { authUser, checkAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, []);
 
+  if (isCheckingAuth) {
+    return (
+      <Center h="100vh" w="full">
+        <Spinner size="md" />
+      </Center>
+    );
+  }
+
   return (
-    <AppProviders>
-      <Routes>
-        <Route path="/" element={<RootLayout />}>
-          <Route
-            path="/"
-            element={authUser ? <Feed /> : <Navigate to="/auth/login" />}
-          />
-          <Route
-            path="/profile"
-            element={authUser ? <Profile /> : <Navigate to="/auth/login" />}
-          />
-          <Route
-            path="/user/:username"
-            element={authUser ? <Profile /> : <Navigate to="/auth/login" />}
-          />
-          <Route
-            path="/messages"
-            element={authUser ? <Messages /> : <Navigate to="/auth/login" />}
-          />
-        </Route>
-        <Route path="/auth" element={<AuthLayout />}>
-          <Route
-            path="/auth/signup"
-            element={!authUser ? <Auth /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/auth/login"
-            element={!authUser ? <Auth /> : <Navigate to="/" />}
-          />
-        </Route>
-      </Routes>
-    </AppProviders>
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route
+          path="/"
+          element={authUser ? <Feed /> : <Navigate to="/auth/login" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <Profile /> : <Navigate to="/auth/login" />}
+        />
+        <Route
+          path="/user/:username"
+          element={authUser ? <Profile /> : <Navigate to="/auth/login" />}
+        />
+        <Route
+          path="/messages"
+          element={authUser ? <Messages /> : <Navigate to="/auth/login" />}
+        />
+      </Route>
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route
+          path="/auth/signup"
+          element={!authUser ? <Auth /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/auth/login"
+          element={!authUser ? <Auth /> : <Navigate to="/" />}
+        />
+      </Route>
+    </Routes>
   );
 };
--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -1,9 +1,12 @@
 import { App } from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AppProviders } from "./AppProviders";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </React.StrictMode>
 );
